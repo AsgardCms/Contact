130f94499348220e093cc5cc8723fd5cd03b9989
Nicolas Widart <n.widart@gmail.com>
2017-10-04 10:54:39 +0200

    Adding extra configuration

--- Config/config.php
+++ Config/config.php
@@ -1,5 +1,20 @@
 <?php
 
 return [
-    'name' => 'Contact'
+    'name' => 'Contact',
+    /*
+    |--------------------------------------------------------------------------
+    | Set the sidebar position of the block menu item
+    |--------------------------------------------------------------------------
+    */
+    'sidebar-position' => 15,
+    /*
+   |--------------------------------------------------------------------------
+   | Custom Sidebar Class
+   |--------------------------------------------------------------------------
+   | If you want to customise the admin sidebar ordering or grouping
+   | You can define your own sidebar class for this module.
+   | No custom sidebar: null
+   */
+    'custom-sidebar' => null,
 ];
